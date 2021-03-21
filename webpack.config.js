e07eccfd087ba98585004a7ba235aabbca10bb87
vinibispo <vini.bispo015@gmail.com>
2021-03-21 06:54:25 -0300

    feat(webpack): Use fast-refresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+
 const isDevelopment = process.env.NODE_ENV !== 'production'
 module.exports = {
   mode: isDevelopment ? 'development' : 'production',
@@ -14,18 +16,27 @@ module.exports = {
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'public'),
+    hot: true,
   },
   plugins: [
     new HTMLWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html'),
-    })
-  ],
+    }),
+    isDevelopment && new ReactRefreshWebpackPlugin()
+  ].filter(Boolean),
   module: {
     rules: [
       {
         test: /(\.jsx$)/,
         exclude: /node_modules/,
-        use: 'babel-loader'
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
+          }
+        }
       },
       {
         test: /(\.scss$)/,
