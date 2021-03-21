c9a37a1bd6ddc382603c5825461f56f496a4f4e8
vinibispo <vini.bispo015@gmail.com>
2021-03-21 06:18:24 -0300

    feat(repository_list): Creating our first component

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,30 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in ReactJS</p>
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in ReactJS</p>
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in ReactJS</p>
+          <a href="">
+            Acessar repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
