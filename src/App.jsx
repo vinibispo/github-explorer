c143948efbd28b7fbb831c496085ad5d6c01a1be
vinibispo <vini.bispo015@gmail.com>
2021-03-21 06:22:01 -0300

    feat(app): Adding src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import {RepositoryList} from './components/RepositoryList'
+export function App() {
+  return (
+    <RepositoryList />
+  )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
-import {StrictMode} from 'react'
-import {render} from 'react-dom'
-function App() {
-  return (
-    <h1>Hello World</h1>
-  )
-}
-
-render(<StrictMode><App/></StrictMode>, document.querySelector('#root'))
+import { StrictMode } from "react";
+import { render } from "react-dom";
+import { App } from "./App";
+render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  document.querySelector("#root")
+);
