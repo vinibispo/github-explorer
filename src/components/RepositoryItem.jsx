d8ddc58869b02a33e5e407404ff9c11e7b059ca4
vinibispo <vini.bispo015@gmail.com>
2021-03-21 06:48:40 -0300

    feat(repository_item): Adding component RepositoryItem

---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,9 @@
+export function RepositoryItem() {
+  return (
+    <li>
+      <strong>unform</strong>
+      <p>Forms in ReactJS</p>
+      <a href="">Acessar repositório</a>
+    </li>
+  );
+}
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,30 +1,12 @@
+import { RepositoryItem } from "./RepositoryItem";
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de Repositórios</h1>
       <ul>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in ReactJS</p>
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in ReactJS</p>
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
-        <li>
-          <strong>unform</strong>
-          <p>Forms in ReactJS</p>
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
+        <RepositoryItem />
       </ul>
     </section>
-  )
+  );
 }
