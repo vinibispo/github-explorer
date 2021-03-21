41da204871641488e4db68b2f4019e5769b7b5e3
vinibispo <vini.bispo015@gmail.com>
2021-03-21 06:32:17 -0300

    feat(sass): Use sass in our application

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import {RepositoryList} from './components/RepositoryList'
+import './styles/global.scss'
 export function App() {
   return (
     <RepositoryList />
--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
         use: 'babel-loader'
       },
       {
-        test: /(\.css$)/,
+        test: /(\.scss$)/,
         exclude: /node_modules/,
         use: [
           'style-loader', 'css-loader', 'sass-loader'
